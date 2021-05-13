bd5289bfd4b9df78f509a42e653078e3d1b11ced
chimdie <nzechimdi@yahoo.com>
2021-05-13 01:16:47 +0100

    configuring local authenticartiion

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const bcrypt = require("bcrypt"); //used to hash passwords
 const app = express();
 const passport = require("passport");
-const session = require('express-session')
-const flash = require('express-flash')
+const session = require("express-session");
+const flash = require("express-flash");
 
 // const saveData = require('./save')
 
-const initializePassport = require('./passport-config')
+const initializePassport = require("./passport-config");
 
-initializePassport(passport, email => users.find(user => user.email))
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email), //find user based on email
+  (id) => users.find((user) => user.id === id)
+);
 
-const users = []; 
+const users = [];
 
 app.set("view-engine", "ejs");
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // secret: key for encryption
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+//Index page Route
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
 
-//Login Route
-app.get("/login", (req, res) => {
+//Login Page Route
+app.get("/login", checkLogin, (req, res) => {
   res.render("login.ejs");
 });
-app.post('/login', passport.authenticate('local', {
-  
-}))
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-//Register Route
-app.get("/register", (req, res) => {
+//Register Page Route
+app.get("/register", checkLogin, (req, res) => {
   res.render("register.ejs");
 });
+
 app.post("/register", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -52,12 +65,27 @@ app.post("/register", async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
     });
-    //Redirect the user to login with the details they just registered with
+    //Redirect the user to login with the details they used to Register
     res.redirect("/login");
   } catch {
     res.redirect("/register");
   }
-  console.log(users);
+  // console.log(users);
 });
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkLogin(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  return next();
+}
+
 
 app.listen(4000);
--- passport-config.js
+++ passport-config.js
@@ -1,24 +1,28 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 
-const initialize = (passport) => {
+const initialize = (passport, getUserEmail, getUserById) => {
   const authenticateUser = async (email, password, done) => {
     const user = getUserEmail(email);
+    //check IF theree is a user
     if (user === null) {
       return done(null, false, { message: "No user with that email found" });
     }
     try {
+      // IF password matches the password entered!
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
         return done(null, false, { message: "Password does not match" });
       }
     } catch (err) {
-      return err;
+      return done(err);
     }
   };
-  passport.use(new LocalStrategy({ userNameField: "email" }), authenticateUser);
-  passport.serializeUser((user, done) => {});
-  passport.deserializeUser((id, done) => {});
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    return  done(null, getUserById(id))
+    });
 };
 module.exports = initialize;
